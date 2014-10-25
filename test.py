5c9124a86a6da8119610abd12925db629039ad22
Alexander Galler <galler.aleksandr@gmail.com>
2014-10-25 17:50:32 +0700

    Correct test.py

--- test.py
+++ test.py
@@ -1,16 +1,24 @@
 import os
+from datetime import datetime
 
 from tpsread import TPS
 
+
 if __name__ == '__main__':
+    print(datetime.now())
     for topdir, dirs, files in sorted(os.walk('./testdata/')):
         for filename in files:
-            if filename.lower().endswith('c.tps'):
+            if filename.lower().endswith('.tps'):
                 print(os.path.join(topdir, filename))
                 # try:
-                tps = TPS(os.path.join(topdir, filename), encoding='cp1251', check=True, current_tablename=b'UNNAMED')
+                tps = TPS(os.path.join(topdir, filename), encoding='cp1251', cached=True, check=True,
+                          current_tablename='UNNAMED')
+                print(datetime.now())
                 for record in tps:
-                    print(record)
+                    # print(record)
+                    pass
+
+    print(datetime.now())
 
                     #unittest
                     #pep8
